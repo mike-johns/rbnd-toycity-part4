076af5707fc6deefcec3d674f2ef1b667c575e21
Mike Johns <hello@mikejohns.email>
2016-07-03 19:42:43 -0700

    DRY out code by moving object vs array logic from #first and #last

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -7,13 +7,13 @@ class Udacidata
     # If the item exists in the database
     # self.new(options)
     # If the item doesn not already exist in the database
-    new_item = self.new(options)
+    new_item = new(options)
     write_to_database(new_item)
     new_item
   end
 
-  def self.new_instance_wrapper(args)
-    self.new :id => args[0], :brand => args[1], :name => args[2], :price => args[3]
+  def self.new_object(args)
+    new :id => args[0], :brand => args[1], :name => args[2], :price => args[3]
   end
 
   def self.read_database
@@ -29,25 +29,25 @@ class Udacidata
   end
 
   def self.all
-    read_database.drop(1).map! { |item| self.new_instance_wrapper(item) }
+    read_database.drop(1).map! { |item| new_object(item) }
+  end
+
+  def self.return_object_or_array(nested_array, count)
+    if count == 1
+      self.new_object(nested_array.flatten!)
+    else
+      nested_array.map! { |item| new_object(item) }
+    end
   end
 
   def self.first(int = 1)
     results = read_database.drop(1).first(int)
-    if int == 1
-      self.new_instance_wrapper(results.flatten!)
-    else
-      results.map! { |item| self.new_instance_wrapper(item) }
-    end
+    return_object_or_array(results, int)
   end
 
   def self.last(int = 1)
     results = read_database.drop(1).last(int)
-    if int == 1
-      self.new_instance_wrapper(results.flatten!)
-    else
-      results.map! { |item| self.new_instance_wrapper(item) }
-    end
+    return_object_or_array(results, int)
   end
 
   def self.find(index)
