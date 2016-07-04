6043e9289820e2ee8558ad7702d68236f34fc0f3
Mike Johns <hello@mikejohns.email>
2016-07-04 13:11:47 -0700

    META TESTS PASSING OMG

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -2,25 +2,12 @@ class Module
   def create_finder_methods(*attributes)
     attributes.each do |attribute|
       instance_eval %Q(
-        def self.find_by_#{attribute}(options = {})
+        def self.find_by_#{attribute}(search_term)
           database = read_database.drop(1)
-          search_object = Product.create(#{attribute} => options[#{attribute}])
-          matching_record = database.find do |record|
-            test_object = Product.create ({ :brand => record[1], :name => record[2] })
-            print test_object
-            print search_object
-            test_object.#{attribute} == search_object.#{attribute}
-          end
+          matching_record = database.find { |record| Product.create({ :brand => record[1], :name => record[2] }).#{attribute} == search_term}
           new_object(matching_record)
         end
         )
     end
-    # instance_eval %Q(
-    #   def self.find_by_brand(brand_name)
-    #     database = read_database.drop(1)
-    #     matching_record = database.find {|record| record[1] == brand_name}
-    #     new_object(matching_record)
-    #   end
-    # )
   end
 end
